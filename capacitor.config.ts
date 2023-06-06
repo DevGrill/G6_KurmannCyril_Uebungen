b16580dc4449c3528dfeabdda1eff944503dc1bf
Cyril Kurmann <cyril.kurmann@gmail.com>
2023-06-06 10:30:13 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G6_KurmannCyril_Uebungen',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
